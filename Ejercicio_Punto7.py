e469a590b505dd577222418abcd86ff09f8c82e3
FranquitoI0 <6tarox@gmail.com>
2024-07-03 15:12:40 -0300

    Add files via upload

---
+++ Ejercicio_Punto7.py
@@ -0,0 +1,2 @@
+print(10/0)
+# me marca que hay error en la division ya que no se puede dividir un numero por cero porque da un resultado indefinido
---
+++ Ejercicio_Punto8.py
@@ -0,0 +1,3 @@
+print((10+3)*(6+6))
+print(10+3)*(6+6)
+# Al sacar los parentesis, el calculo se mezcla y el sistema no sabe que parte esta dentro de la operacion y que no
